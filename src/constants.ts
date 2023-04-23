2c875de5890922fc05a1b878939c77dce14cbd0a
Govind Karki <govind050796@gmail.com>
2023-04-23 17:42:29 +0530

    add socket connection setup

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const constants = {
+  socketEvents: {
+    USER_LIST: 'user-list',
+    USER_JOINED: 'user-joined',
+    USER_LEFT: 'user-left',
+    USER_UPDATE: 'user-update',
+    MOUSE_POS_UPDATE: 'mouse-pos-update',
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,69 @@
+import * as express from 'express';
+import * as http from 'http';
+import { Server } from 'socket.io';
+import { constants } from './constants';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.json(
+    'This is the realtime flow builder backend. Use websocket to connect to this service'
+  );
+});
+
+server.listen(8088, () => {
+  console.log('Listening on Port: 8088');
+});
+
+const rooms: {
+  [roomId: string]: {
+    [socketId: string]: { id: string; name: string; mousePos: object };
+  };
+} = {};
+
+io.on('connection', socket => {
+  const { name, roomId } = <{ name: string; roomId: string }>(
+    socket.handshake.query
+  );
+
+  // join the room and get the list of all users in that room
+  socket.join(roomId);
+  if (!rooms[roomId]) rooms[roomId] = {};
+
+  io.to(socket.id).emit(
+    constants.socketEvents.USER_LIST,
+    Object.values(rooms[roomId])
+  );
+
+  // create a socket entry in rooms object
+
+  rooms[roomId][socket.id] = { id: socket.id, name, mousePos: {} };
+
+  // emit to others in the room about new user joined
+  socket
+    .to(roomId)
+    .emit(constants.socketEvents.USER_JOINED, rooms[roomId][socket.id]);
+
+  // emit to others in the room when this user leaves and delete entry from rooms
+  socket.on('disconnect', () => {
+    socket
+      .to(roomId)
+      .emit(constants.socketEvents.USER_LEFT, rooms[roomId][socket.id]);
+    delete rooms[roomId][socket.id];
+  });
+
+  socket.on(constants.socketEvents.MOUSE_POS_UPDATE, data => {
+    rooms[roomId][socket.id].mousePos = data;
+
+    socket
+      .to(roomId)
+      .emit(constants.socketEvents.USER_UPDATE, rooms[roomId][socket.id]);
+  });
+});
